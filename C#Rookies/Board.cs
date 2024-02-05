1b436b6f84601a50e687ab1136a59917b2de7b3e
ManoftheStreet <zhijian6527@naver.com>
2024-02-05 17:27:17 +0900

    first commit

---
+++ C#Rookies/Board.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Rookies
+{
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+        public TileType[,] Tile { get; private set; }
+        public int Size { get; private set; }
+
+        Player _player;
+
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void initialize(int size, Player player)
+        {
+            if(size %2 == 0)
+            {
+                return;
+            }
+
+            _player = player;
+
+            Tile = new TileType[size, size];
+            Size = size;
+
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
+        }
+
+        void GenerateBySideWinder()
+        {
+            //모든길 막음
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        Tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        Tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            //랜덤으로 길 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                int cnt = 1;
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2)
+                        continue;
+
+                    if (y == Size - 2)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == Size - 2)
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        cnt++;
+                    }
+                    else
+                    {
+                        int randIndex = rand.Next(0, cnt);
+                        Tile[y + 1, x - randIndex * 2] = TileType.Empty;
+                        cnt = 1;
+                    }
+                }
+            }
+        }
+
+        void GenerateByBinaryTree()
+        {
+            //모든길 막음
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        Tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        Tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            //랜덤으로 길 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2)
+                        continue;
+
+                    if (y == Size - 2)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == Size - 2)
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                    }
+
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor preColor = Console.ForegroundColor;
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    //플레이어 좌표와 비교해서 일치하면 플레이어 색상으로 설정
+                    if(y==_player.PosY && x == _player.PosX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = GetTileColor(Tile[y, x]);
+                    }
+                    
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = preColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+        }
+    }
+}
---
+++ C#Rookies/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Xml;
+
+namespace C_Rookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+
+            board.initialize(25, player);
+            player.Initialize(1, 1, board.Size - 2, board.Size - 2, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                //입력
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+
+                //로직
+                player.Update(deltaTick);
+                //렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
